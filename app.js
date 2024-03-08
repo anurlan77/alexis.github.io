7a91939c5e1173e89fb59691295fde10c35d9e16
anurlan77 <49644195+anurlan77@users.noreply.github.com>
2024-03-08 23:16:55 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".testim__feedbacks").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
+});
